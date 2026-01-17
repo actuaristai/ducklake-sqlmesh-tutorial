dcafe9a917267b3c7540a2227f5aa1d223e8719e
actuaristai <wil.lee.am+github@gmail.com>
2026-01-17 16:08:52 +1100

    feat: using defined catalog name my_lakehouse

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 Taken from https://www.tobikodata.com/blog/ducklake-sqlmesh-tutorial-a-hands-on
-main change to make it work is to set the catalogs to be called catalog in config.yaml (instead of my_lakehouse in the website)
 
 # Pre-requesities
 
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ ibis.options.interactive = True
 # use this code snippet to show the connection pane on the right. note need to diconnect to reduce conflicts.
 connection_pane = duckdb.connect()
 connection_pane.install_extension("ducklake")
-connection_pane.sql("ATTACH 'ducklake:data/catalog.ducklake'")
+connection_pane.sql("ATTACH 'ducklake:data/catalog.ducklake' AS my_lakehouse")
 %connection_show connection_pane
 connection_pane.close()
 
@@ -15,14 +15,15 @@ connection_pane.close()
 def main():
     print("Showing example of how to use with ibis")
     con = ibis.duckdb.connect(extensions="ducklake")
-    con.attach("ducklake:data/catalog.ducklake", read_only=True)
-    events = con.table('events', database='catalog.raw')
-    stg_events = con.table('stg_events', database='catalog.staging')
-    daily_revenue = con.table('daily_revenue', database='catalog.analytics')
-    con.list_tables(database='catalog.raw')
-    con.list_tables(database='catalog.staging')
-    con.list_tables(database='catalog.analytics')
-    con.list_databases(catalog='catalog')
+    con.attach(path="ducklake:data/catalog.ducklake", name='my_lakehouse')
+    events = con.table('events', database='my_lakehouse.raw')
+    stg_events = con.table('stg_events', database='my_lakehouse.staging')
+    daily_revenue = con.table(
+        'daily_revenue', database='my_lakehouse.analytics')
+    con.list_tables(database='my_lakehouse.raw')
+    con.list_tables(database='my_lakehouse.staging')
+    con.list_tables(database='my_lakehouse.analytics')
+    con.list_databases(catalog='my_lakehouse')
     con.list_catalogs()
     con.disconnect()
 
